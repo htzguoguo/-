2b279a7af8124a8495b8eb519ff71db990b7d420
Administrator <binnorman@139.com>
2016-11-15 20:27:39 +0800

    项目初始化完成

---
+++ src/js/tjx.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Administrator on 2016/11/15.
+ */
+
+var tjx = {};
+
+
---
+++ test/hello_spec.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2016/11/15.
+ */
+
+describe( 'Hello', function (  ) {
+    it( 'say hello to world', function (  ) {
+        expect( sayHello() ).toBe( 'Hello World!' );
+    } );
+} );
